2064700578ac1f506557b09585d7a890f0cab2b8
Huan Gan <Huan Gan>
2017-09-14 14:51:53 +0800

    zuul server配置成功

--- mirana-zuul/src/main/java/org/mirana/cloud/zuul/ZuulServerApplication.java
+++ mirana-zuul/src/main/java/org/mirana/cloud/zuul/ZuulServerApplication.java
@@ -1,10 +1,9 @@
 package org.mirana.cloud.zuul;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 /**
  * 
@@ -14,14 +13,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * @version  1.0
  * 开发日期：2017年9月14日 ： 上午10:41:02
  */
-@EnableZuulProxy
-@EnableEurekaClient
-@EnableDiscoveryClient
 @SpringBootApplication
+//@Controller
+@EnableZuulProxy
 public class ZuulServerApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulServerApplication.class, args);
+		new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
 	}
 	
 }
